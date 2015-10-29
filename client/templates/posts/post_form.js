1d79b5d78c18e25cbecd0504d9b6e4b5d90e576d
Gilbert <gyim@hawaii.edu>
2015-10-28 22:20:43 -1000

    Finished practice WOD

---
+++ client/templates/posts/post_form.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Gilbert on 10/28/2015.
+ */
+Template.postForm.events({
+
+  // handle the form submission
+  'submit form': function(event, template) {
+
+    // stop the form from submitting
+    event.preventDefault();
+
+    Posts.insert({
+      title: event.target.title.value,
+      url: event.target.url.value
+    });
+    template.find("form").reset();
+  }
+
+});
